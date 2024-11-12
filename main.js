16e0ab6c19cedc2a2e989ae929a1373f18b66395
KATO KOKORO <koko.rila.928@gmail.com>
2024-11-12 20:26:49 +0900

    aa

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 {
-  const hl = document.querySelector('#h-u');
   const ham = document.querySelector('#js-hamburger');
   const meu = document.querySelector('#clact');
   ham.addEventListener('click', function () {
     ham.classList.toggle('active');
     meu.classList.toggle('active');
-    hl.classList.add('active');
   });
-
 }
