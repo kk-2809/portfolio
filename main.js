08db8a545e1d37939b91e7d9bc6c1e96df917ef3
KATO KOKORO <koko.rila.928@gmail.com>
2024-11-19 22:28:05 +0900

    aa

--- main.js
+++ main.js
@@ -6,3 +6,28 @@
     meu.classList.toggle('active');
   });
 }
+
+{
+  const currentActive = document.querySelectorAll('#menu');
+    currentActive.addEventListener('click',e => {
+      e.preventDefault();
+      if (currentActive.classList.contains('active') == true) {
+        currentActive.classList.remove('active');
+      } return;
+    });
+}
+
+
+// name
+{
+  const CLASSNAME = "-visible";
+  const TIMEOUT = 1500;
+  const $target = $(".title");
+
+  setInterval(() => {
+    $target.addClass(CLASSNAME);
+    setTimeout(() => {
+      $target.removeClass(CLASSNAME);
+    }, TIMEOUT);
+  }, TIMEOUT * 2);
+}
