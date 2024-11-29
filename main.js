508f99291a5673ff93d6b2e184d6ef186dcb8d30
KATO KOKORO <koko.rila.928@gmail.com>
2024-11-29 14:46:04 +0900

    aa

--- main.js
+++ main.js
@@ -24,18 +24,18 @@
 
 
 // name
-{
-  const CLASSNAME = "-visible";
-  const TIMEOUT = 1500;
-  const $target = $(".title");
+// {
+//   const CLASSNAME = "-visible";
+//   const TIMEOUT = 1500;
+//   const $target = $(".title");
 
-  setInterval(() => {
-    $target.addClass(CLASSNAME);
-    setTimeout(() => {
-      $target.removeClass(CLASSNAME);
-    }, TIMEOUT);
-  }, TIMEOUT * 2);
-}
+//   setInterval(() => {
+//     $target.addClass(CLASSNAME);
+//     setTimeout(() => {
+//       $target.removeClass(CLASSNAME);
+//     }, TIMEOUT);
+//   }, TIMEOUT * 2);
+// }
 
 
 
