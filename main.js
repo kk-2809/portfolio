734932c15f17569e275225be0f69ba07d58328ea
KATO KOKORO <koko.rila.928@gmail.com>
2024-11-29 14:18:01 +0900

    aa

--- main.js
+++ main.js
@@ -38,96 +38,26 @@
 }
 
 
-// graph
-// {
-//   const objects = document.querySelectorAll('.scroll-in');
-//   const cb = function (entries, observer) {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         entry.target.classList.add('displayed');
-//         observer.unobserve(entry.target);
-//       };
-//     });
-//   }
-
-//   const options = {
-//     root: null,
-//     rootMargin: "0px",
-//     threshold: 0
-//   }
-
-//   const io = new IntersectionObserver(cb, options);
-//   objects.forEach(object => {
-//     io.observe(object);
-//   });
-// }
 
 
-// {
-//   function callback(entrys,obs){
-//     entrys.forEach(entry =>{
-//       if(!entry.isIntersecting){
-//         return;
-//       }
-//       entry.target.classList.add("appear");
-//       obs.unobserve(entry.target);
-//     });
-//   }
-//   const options = {
-//     threshold:0.2,
-//   };
-//   const observer = new IntersectionObserver(callback, options);
-//   const targets = document.querySelectorAll(".feature");
-
-//   targets.forEach(target =>{
-//     observer.observe(target);
-//   });
-// }
-
+//graph
 {
-  function callback(entrys,obs){
-    entrys.forEach(entry =>{
-      if(!entry.isIntersecting){
+  function callback(entrys, obs) {
+    entrys.forEach(entry => {
+      if (!entry.isIntersecting) {
         return;
       }
-      entry.target.classList.add("appear");
+      entry.target.classList.add("app");
       obs.unobserve(entry.target);
     });
-  }
-  const options = {
-    threshold:0.2,
   };
-  const observer = new IntersectionObserver(callback, options);
-  const targets = document.querySelectorAll(".graphs1");
+    const options = {
+      threshold: 0.2,
+    };
+    const observer = new IntersectionObserver(callback, options);
+    const targets = document.querySelectorAll(".graph");
 
-  targets.forEach(target =>{
-    observer.observe(target);
-  });
+    targets.forEach(target =>{
+      observer.observe(target);
+    });
 }
-
-// {
-//   function scrollCallBack(entrys){
-//     entrys.forEach(entry =>{
-//       if(!entry.isIntersecting){
-//         header.classList.add("scrolled");
-//         toTop.classList.add("scrolled");
-//       }else{
-//         header.classList.remove("scrolled");
-//         toTop.classList.remove("scrolled"); 
-//       }
-//     });
-//   }
-//   const toTop = document.getElementById("to_top");
-
-//   toTop.addEventListener("click", e =>{
-//     e.preventDefault();
-//     window.scrollTo({
-//       top:0,
-//       behavior:"smooth",
-//     });
-//   });
-
-//   const header = document.querySelector("header");
-//   const scrollObserver = new IntersectionObserver(scrollCallBack);
-//   scrollObserver.observe(document.getElementById("target-2"));
-// }
