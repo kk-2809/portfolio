b446f3368f02f20fa97a993716e2e76592858107
KATO KOKORO <koko.rila.928@gmail.com>
2024-12-04 18:53:59 +0900

    aa

--- main.js
+++ main.js
@@ -61,3 +61,27 @@
       observer.observe(target);
     });
 }
+
+
+//scroll
+{
+const pagetopBtn = document.querySelector('.to-pagetop');
+
+toScrollTop = () => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+const scrollEvents = () => {
+  if (window.scrollY > 100) {
+    pagetopBtn.classList.add('is-shown');
+  } else if (window.scrollY < 100) {
+    pagetopBtn.classList.remove('is-shown');
+  }
+}
+
+pagetopBtn.addEventListener('click', toScrollTop);
+window.addEventListener('scroll', scrollEvents);
+}
