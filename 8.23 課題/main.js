d15710da591bac9624232f6e94af03f4b15ae3da
KATO KOKORO <koko.rila.928@gmail.com>
2024-12-22 21:20:50 +0900

    c

--- 8.23 課題/main.js
+++ 8.23 課題/main.js
@@ -1,3 +1,26 @@
+//詳細ボタン
+
+{
+  const btn3 = document.getElementById('btn3');
+  const mask = document.getElementById('mask');
+  const modal = document.getElementById('modal');
+
+  btn3.addEventListener('click', () => {
+    mask.classList.remove('hidden');
+    modal.classList.remove('hidden');
+  });
+
+  mask.addEventListener('click', () => {
+    mask.classList.add('hidden');
+    modal.classList.add('hidden');
+  });
+}
+
+
+
+
+//問い合わせ
+
 {
   const pagetopBtn = document.querySelector('.to-pagetop');
 
